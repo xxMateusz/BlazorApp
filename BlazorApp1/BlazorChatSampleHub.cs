31ebf8eb332fac6939da80b0df0cffc7318bc043
xxMateusz <mateusz.bochenek@microsoft.wsei.edu.pl>
2024-03-13 16:07:38 +0100

    Dodaj pliki projektów.

---
+++ BlazorApp1/BlazorChatSampleHub.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorApp1
+{
+    public class BlazorChatSampleHub: Hub
+    {
+        public const string HubUrl = "/chat";
+        private readonly UserService _userService;
+
+        public BlazorChatSampleHub(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task Broadcast(string username, string message)
+        {
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(e);
+        }
+        public async Task Private(string from, string to, string message)
+        {
+            var connectionId = _userService.GetConnectioIdByName(to);
+            if (connectionId != null)
+            {
+                await Clients.Client(connectionId).SendAsync("Private", from, message);
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("Error", $"User '{to}' is not available.");
+            }
+        }
+
+
+    }
+}
---
+++ BlazorApp1/UserService.cs
@@ -0,0 +1,33 @@
+﻿namespace BlazorApp1
+{
+    public class UserService
+    {
+        private readonly Dictionary<string, string> _userConnections = new Dictionary<string, string>();
+
+        public void Add(string connectionId, string username)
+        {
+            _userConnections[username] = connectionId;
+        }
+
+        public void RemoveByName(string username)
+        {
+            if (_userConnections.ContainsKey(username))
+            {
+                _userConnections.Remove(username);
+            }
+        }
+
+        public string GetConnectioIdByName(string username)
+        {
+            return _userConnections.ContainsKey(username) ? _userConnections[username] : null;
+        }
+
+        public IEnumerable<(string ConnectionId, string Username)> GetAll()
+        {
+            foreach (var pair in _userConnections)
+            {
+                yield return (pair.Value, pair.Key);
+            }
+        }
+    }
+}
